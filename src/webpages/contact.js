5df51bd3b4e17d1758888ec907d35e61183289f8
Isaac Tan <isaac@graduate.utm.my>
2021-08-08 23:02:27 +0800

    contact page

--- src/webpages/contact.js
+++ src/webpages/contact.js
@@ -1,11 +1,29 @@
 import React from 'react';
 const Contact = () => {
+    function copyClip() {
+        var copy = document.getElementById('email');
+        copy.select();
+        copy.setSelectionRange(0, 99999);
+
+        document.execCommand("copy");
+
+        alert("Copied " + copy.value);
+    }
     return (
         <header className="App-header">
-            <div>
-            <h1>Contact Page</h1>
-            <p>This is Contact page</p>
-        </div>
+            <div className="form-group">
+                <h1>Contact Page</h1>
+                <br/>
+                <div>
+                    <div className="form-group">
+                        <label className="form-label">Email: </label>
+                        <input className="form-control" value="iztan98@gmail.com" type="text" id="email" readOnly />
+                    </div>
+                    <div>
+                        <button className="btn btn-primary" onClick={copyClip}>Copy ME</button>
+                    </div>
+                </div>
+            </div>
         </header>
     );
 };
