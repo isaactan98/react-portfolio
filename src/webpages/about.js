f5a521dd3c4ce020ae59e8c144a1fe24de591994
Isaac Tan <isaac@graduate.utm.my>
2021-08-18 21:27:40 +0800

    update css for about page

--- src/webpages/about.js
+++ src/webpages/about.js
@@ -5,10 +5,14 @@ const About = () => {
             <div>
                 <div className="about-header">
                     <div>
-                        <h3>Hi there, I'm Isaac Tan here</h3>
+                        <h3>Hi there, I'm <span className="flicker-1">Isaac Tan</span> here</h3>
                     </div>
                     <div className="about-body">
-                        <h5>Currently, I am studying at <a className="wobble-hor-bottom" href="https://www.utm.my/">Universiti Teknologi Malaysia</a> as a <a className="wobble-hor-bottom" href="https://en.wikipedia.org/wiki/Software_engineer">Software Engineering</a> Student.</h5>
+                        <h5><div>Currently, I am studying at <a className="wobble-hor-bottom" href="https://www.utm.my/">Universiti Teknologi Malaysia</a> as a <a className="wobble-hor-bottom" href="https://en.wikipedia.org/wiki/Software_engineer">Software Engineering</a> Student.</div></h5>
+                    </div>
+                    <hr/>
+                    <div>
+
                     </div>
                 </div>
             </div>
